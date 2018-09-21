477adebcf7b7c32c2fb3de6a647b0debab43ca9a
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-09-21 11:40:16 +0530

    Add placeholders for components

---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const WeatherCard = () => <h1>Weather App</h1>
+
+export default WeatherCard
---
+++ src/components/WeatherCardGroup.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import WeatherCard from './WeatherCard'
+
+const WeatherCardGroup = () => <h1>Weather App</h1>
+
+export default WeatherCardGroup
