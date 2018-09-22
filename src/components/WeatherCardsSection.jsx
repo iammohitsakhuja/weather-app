fafab76a9fb8868b3f90fe343202e14d34933393
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-09-22 23:41:09 +0530

    Use props for displaying data on card

--- src/components/WeatherCardsSection.jsx
+++ src/components/WeatherCardsSection.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 import WeatherCard from './WeatherCard'
 
-const WeatherCardsSection = () => <WeatherCard />
+const cardStyle = {
+  backgroundColor: '#FFB100',
+  width: 500,
+  height: 160,
+  color: '#FFFFFF',
+  borderRadius: 3,
+  fontSize: 25,
+  fontWeight: 'bold',
+}
+
+const weatherIcon = {
+  float: 'right',
+  marginRight: 20,
+  height: 50,
+  width: 50,
+  marginBottom: 15,
+}
+
+const weatherInfo = {
+  location: {
+    city: 'Chandigarh',
+    country: 'India',
+  },
+  temperature: 36,
+  time: new Date(),
+}
+
+const WeatherCardsSection = () => (
+  <WeatherCard cardStyle={cardStyle} weatherIcon={weatherIcon} weatherInfo={weatherInfo} />
+)
 
 export default WeatherCardsSection
