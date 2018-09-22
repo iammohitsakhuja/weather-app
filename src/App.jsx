748fca5e21fa6eb023c684bdfdfa8728c3ab3a42
Ravi Kant Garg <ravii.garg24@gmail.com>
2018-09-22 21:19:46 +0530

    Add weather card

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
 
-import logo from './logo.svg'
 import './styles/App.css'
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-)
+import WeatherCard from './components/WeatherCard'
+
+const App = () => <div className="App">{<WeatherCard />}</div>
 
 export default App
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,5 +1,72 @@
 import React from 'react'
+import 'antd/dist/antd.css'
+import { Card, Row, Col } from 'antd'
+import Sun from '../sun.png'
 
-const WeatherCard = () => <h1>Weather App</h1>
+const styleCard = {
+  backgroundColor: '#FFB100',
+  width: 500,
+  height: 160,
+  color: '#FFFFFF',
+  borderRadius: 3,
+  fontSize: 25,
+  fontWeight: 'bold',
+}
+
+const leftColStyle = {
+  marginLeft: 15,
+  float: 'left',
+}
+
+const rightColStyle = {
+  marginRight: 15,
+  float: 'right',
+  fontSize: 20,
+}
+
+const whetherIcon = {
+  float: 'right',
+  marginRight: 20,
+  height: 50,
+  width: 50,
+  marginBottom: 15,
+}
+
+const WeatherCard = () => (
+  <Card bordered={false} style={styleCard}>
+    <Row type="flex" justify="start">
+      <Col span={12}>
+        <p style={leftColStyle}>
+          Chandigarh, &nbsp;
+          <span style={{ fontSize: 15 }}>India</span>
+        </p>
+      </Col>
+      <Col span={12}>
+        <img alt="sun" src={Sun} style={whetherIcon} />
+      </Col>
+    </Row>
+
+    <Row type="flex" justify="start">
+      <Col span={12}>
+        <p
+          style={{
+            fontSize: 35,
+            marginLeft: 20,
+            float: 'left',
+          }}
+        >
+          36&#x2103;
+        </p>
+      </Col>
+      <Col span={12}>
+        <p style={rightColStyle}>
+          12:18 <span style={{ fontSize: 15 }}>PM</span>
+          <br />
+          <span style={{ fontSize: 15 }}>Wednesday</span>
+        </p>
+      </Col>
+    </Row>
+  </Card>
+)
 
 export default WeatherCard
