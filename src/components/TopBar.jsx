c702b0d0371720825a4c51596a6a4d603f5582e4
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-09-23 17:18:03 +0530

    Auto complete working

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-
-import { AutoComplete, Input, Layout, Button, Row, Col } from 'antd'
+import React, { Component } from 'react'
+import axios from 'axios'
+import { AutoComplete, Button, Col, Icon, Input, Layout, Row } from 'antd'
 
 const { Header } = Layout
+const { Option } = AutoComplete
 
-// brand styling
+const { REACT_APP_AUTOCOMPLETE_URI } = process.env
+
+// Brand styling.
 const topBarBrandStyle = {
   color: 'White',
   fontFamily: 'Spirax',
@@ -12,22 +15,61 @@ const topBarBrandStyle = {
   textAlign: 'center',
 }
 
-const TopBar = () => (
-  <Header>
-    <Row>
-      <Col span={8}>
-        <AutoComplete placeholder="City, state, country">
-          <Input suffix={<Button type="primary" icon="search" style={{ marginRight: -13 }} />} />
-        </AutoComplete>
-      </Col>
-      <Col span={8} style={topBarBrandStyle}>
-        Weather
-      </Col>
-      <Col span={8} style={{ textAlign: 'end' }}>
-        <Button type="primary" icon="tool" shape="circle" />
-      </Col>
-    </Row>
-  </Header>
-)
+class TopBar extends Component {
+  state = {
+    cities: [],
+  }
+
+  handleSearch = async value => {
+    const requestURL = `${REACT_APP_AUTOCOMPLETE_URI}&input=${value}`
+
+    try {
+      const response = await axios.get(requestURL)
+      const { predictions } = response.data
+
+      const cities = predictions.map(prediction => (
+        <Option key={prediction.id} value={prediction.place_id} description={prediction.description}>
+          {prediction.description}
+        </Option>
+      ))
+
+      this.setState({ cities })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  handleSelect = async value => {
+    console.log('onSelect', value)
+  }
+
+  render() {
+    const { cities } = this.state
+
+    return (
+      <Header>
+        <Row>
+          <Col span={8}>
+            <AutoComplete
+              dataSource={cities}
+              onSelect={this.handleSelect}
+              onSearch={this.handleSearch}
+              optionLabelProp="description"
+              placeholder="City, state, country"
+            >
+              <Input suffix={<Icon type="search" />} />
+            </AutoComplete>
+          </Col>
+          <Col span={8} style={topBarBrandStyle}>
+            Weather
+          </Col>
+          <Col span={8} style={{ textAlign: 'end' }}>
+            <Button type="primary" icon="tool" shape="circle" />
+          </Col>
+        </Row>
+      </Header>
+    )
+  }
+}
 
 export default TopBar
