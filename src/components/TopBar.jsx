d5fba7f0e0873ec79bdaad5a9476fbd09f8157ea
Manuinder Sekhon <sekhon.manuinder@gmail.com>
2018-09-23 12:22:53 +0530

    Added top-bar search (#12)
    * add babel
    
    * top bar
    
    * vscode-large-workspace fix
    
    * static top-bar
    
    * search top-bar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,5 +1,33 @@
 import React from 'react'
 
-const TopBar = () => <h1>Weather App</h1>
+import { AutoComplete, Input, Layout, Button, Row, Col } from 'antd'
+
+const { Header } = Layout
+
+// brand styling
+const topBarBrandStyle = {
+  color: 'White',
+  fontFamily: 'Spirax',
+  fontSize: 40,
+  textAlign: 'center',
+}
+
+const TopBar = () => (
+  <Header>
+    <Row>
+      <Col span={8}>
+        <AutoComplete placeholder="City, state, country">
+          <Input suffix={<Button type="primary" icon="search" style={{ marginRight: -13 }} />} />
+        </AutoComplete>
+      </Col>
+      <Col span={8} style={topBarBrandStyle}>
+        Weather
+      </Col>
+      <Col span={8} style={{ textAlign: 'end' }}>
+        <Button type="primary" icon="tool" shape="circle" />
+      </Col>
+    </Row>
+  </Header>
+)
 
 export default TopBar
