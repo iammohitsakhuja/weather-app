20105a7e70c62a1131d2875b0c48a3e518759765
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-09-23 19:41:39 +0530

    Auto complete working (#13)
    * Auto complete working
    
    * Log latitude and longitude (#14)
    
    * Place details (#15)
    
    * Log latitude and longitude
    
    * Remove redundant code
    
    * Switch API provider

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-
-import { AutoComplete, Input, Layout, Button, Row, Col } from 'antd'
+import React, { Component } from 'react'
+import axios from 'axios'
+import { AutoComplete, Button, Col, Icon, Input, Layout, Row } from 'antd'
 
 const { Header } = Layout
+const { Option } = AutoComplete
 
-// brand styling
+// Get environment variables.
+const { REACT_APP_AUTOCOMPLETE_URI, REACT_APP_PLACE_DETAILS_URI } = process.env
+
+// Brand styling.
 const topBarBrandStyle = {
   color: 'White',
   fontFamily: 'Spirax',
@@ -12,22 +16,74 @@ const topBarBrandStyle = {
   textAlign: 'center',
 }
 
-const TopBar = () => (
-  <Header>
-    <Row>
-      <Col span={8}>
-        <AutoComplete placeholder="City, state, country">
-          <Input suffix={<Button type="primary" icon="search" style={{ marginRight: -13 }} />} />
-        </AutoComplete>
-      </Col>
-      <Col span={8} style={topBarBrandStyle}>
-        Weather
-      </Col>
-      <Col span={8} style={{ textAlign: 'end' }}>
-        <Button type="primary" icon="tool" shape="circle" />
-      </Col>
-    </Row>
-  </Header>
-)
+class TopBar extends Component {
+  state = {
+    cities: [],
+  }
+
+  handleSearch = async value => {
+    const requestURI = `${REACT_APP_AUTOCOMPLETE_URI}&query=${value}`
+    try {
+      const response = await axios.get(requestURI)
+      const { suggestions } = response.data
+
+      if (suggestions !== undefined) {
+        const cities = suggestions.map(suggestion => (
+          <Option key={suggestion.locationId} value={suggestion.locationId} label={suggestion.label}>
+            {suggestion.label}
+          </Option>
+        ))
+
+        this.setState({ cities })
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  handleSelect = async locationId => {
+    const requestURI = `${REACT_APP_PLACE_DETAILS_URI}&locationid=${locationId}`
+
+    try {
+      const response = await axios.get(requestURI)
+      const { Latitude, Longitude } = response.data.Response.View[0].Result[0].Location.DisplayPosition
+
+      console.log(Latitude)
+      console.log(Longitude)
+
+      // TODO: Add card.
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  render() {
+    const { cities } = this.state
+
+    return (
+      <Header>
+        <Row>
+          <Col span={8}>
+            <AutoComplete
+              dataSource={cities}
+              onSelect={this.handleSelect}
+              onSearch={this.handleSearch}
+              optionLabelProp="label"
+              placeholder="City, state, country"
+            >
+              <Input suffix={<Icon type="search" />} />
+            </AutoComplete>
+          </Col>
+          <Col span={8} style={topBarBrandStyle}>
+            Weather
+          </Col>
+          <Col span={8} style={{ textAlign: 'end' }}>
+            <Button type="primary" icon="tool" shape="circle" />
+          </Col>
+        </Row>
+      </Header>
+    )
+  }
+}
 
 export default TopBar
