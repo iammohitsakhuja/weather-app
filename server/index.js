2c34cc288510526b538e241f589a82ae5a68ba1e
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-11-01 09:58:56 +0530

    Separate server and client into workspaces (#37)

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import logger from 'morgan'
+import bodyParser from 'body-parser'
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(logger('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Error handler.
+app.use((err, req, res, next) => res.status(err.status || 500).send(err.message || 'There was a problem'))
+
+// Start the server.
+app.listen(PORT, () => {
+  console.log(`Express app listening on port: ${PORT}`)
+})
