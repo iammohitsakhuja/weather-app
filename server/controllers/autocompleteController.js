0226d773268c428b9c219ffc4088c8efb823664b
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-11-01 16:57:55 +0530

    Better logging

--- server/controllers/autocompleteController.js
+++ server/controllers/autocompleteController.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import debug from 'debug'
 
 // Get the api keys and codes from the environment.
 const { HERE_MAPS_API_ID, HERE_MAPS_APP_CODE } = process.env
@@ -7,6 +8,8 @@ const { HERE_MAPS_API_ID, HERE_MAPS_APP_CODE } = process.env
 // Generate autocomplete URI using the API key and app code.
 const autocompleteURI = `https://autocomplete.geocoder.api.here.com/6.2/suggest.json?&app_id=${HERE_MAPS_API_ID}&app_code=${HERE_MAPS_APP_CODE}`
 /* eslint-enable */
+
+const customLogger = debug('autocomplete')
 
 const getAutocompleteSuggestions = async (req, res, next) => {
   if (req.query.value.length === 0) return res.send({})
@@ -18,7 +21,7 @@ const getAutocompleteSuggestions = async (req, res, next) => {
 
     return res.send(response.data)
   } catch (err) {
-    console.log(err)
+    customLogger(err)
     return next(err)
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import logger from 'morgan'
 import bodyParser from 'body-parser'
+import debug from 'debug'
 
 import './configureEnvironment'
 import router from './routes'
 
+const name = 'Weather App'
+
 const app = express()
 const PORT = process.env.PORT || 3001
+const customLogger = debug('app')
 
 app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -20,5 +24,5 @@ app.use((err, req, res, next) => res.status(err.status || 500).send(err.message 
 
 // Start the server.
 app.listen(PORT, () => {
-  console.log(`Weather app server listening on port: ${PORT}`)
+  customLogger('%o server listening on port: %o', name, PORT)
 })
