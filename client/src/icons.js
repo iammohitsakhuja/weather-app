e7a01957f7980fa8b46b82cc9ea01b5e7ec3da6d
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-11-04 22:13:13 +0530

    More responsive styling and static icons
    - Includes more responsive styling for expanded weather card
    - Better scaling for font
    - More use of whitespace
    - Use of static icons for Weather attributes like: temperature

--- client/src/icons.js
+++ client/src/icons.js
@@ -3,30 +3,28 @@
  * To ensure consistency, import icons from this file only.
  */
 
-import ClearNight from './img/icons/clear-night.png'
-import CloudyDay from './img/icons/cloudy-day.png'
-import CloudyGeneral from './img/icons/cloudy-general.png'
-import CloudyNight from './img/icons/cloudy-night.png'
-import MistyGeneral from './img/icons/misty-general.png'
-import RainShowers from './img/icons/rain-showers.png'
-import RainyDay from './img/icons/rainy-day.png'
-import RainyNight from './img/icons/rainy-night.png'
-import ScatteredClouds from './img/icons/scattered-clouds.png'
-import SnowyGeneral from './img/icons/snowy-general.png'
-import SunnyDay from './img/icons/sunny-day.png'
-import ThunderstormGeneral from './img/icons/thunderstorm-general.png'
+import Humidity from './assets/icons/humidity.svg'
+import NewMoon from './assets/icons/moon-0.svg'
+import WaxingCrescent from './assets/icons/moon-1.svg'
+import WaxingGibbous from './assets/icons/moon-2.svg'
+import FullMoon from './assets/icons/moon-3.svg'
+import WaningGibbous from './assets/icons/moon-4.svg'
+import WaningCrescent from './assets/icons/moon-5.svg'
+import Rain from './assets/icons/rain.svg'
+import TemperatureCold from './assets/icons/temperature-cold.svg'
+import TemperatureHot from './assets/icons/temperature-hot.svg'
+import Wind from './assets/icons/wind.svg'
 
-export {
-  ClearNight,
-  CloudyDay,
-  CloudyGeneral,
-  CloudyNight,
-  MistyGeneral,
-  RainShowers,
-  RainyDay,
-  RainyNight,
-  ScatteredClouds,
-  SnowyGeneral,
-  SunnyDay,
-  ThunderstormGeneral,
+export default {
+  Humidity,
+  NewMoon,
+  WaxingCrescent,
+  WaxingGibbous,
+  FullMoon,
+  WaningGibbous,
+  WaningCrescent,
+  Rain,
+  TemperatureCold,
+  TemperatureHot,
+  Wind,
 }
