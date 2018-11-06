91d39bb9136e27f8b20f7530fe5bfb5c3e7a6047
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-11-06 22:36:43 +0530

    Factor out hourly forecast and make it dynamic

--- client/src/components/ExpandedWeatherCard.jsx
+++ client/src/components/ExpandedWeatherCard.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import moment from 'moment'
 
 import AnimatedWeatherIconsReact from './AnimatedWeatherIconsReact'
+import HourlyForecastSection from './HourlyForecastSection'
 import DailyForecastSection from './DailyForecastSection'
 import WeatherAttributesSection from './WeatherAttributesSection'
 import { getLocationData } from '../reducers/locations'
@@ -14,114 +15,96 @@ const getTemperature = temperature => parseInt(temperature)
 const getIconName = iconName => iconName.toUpperCase().replace(/-/g, '_')
 const getFormattedDate = time => moment.unix(time).format('MMMM Do, YYYY')
 
+const getHourlyData = (hourlyData, lastRefreshTime, gap = 3, maxItems = 5) => {
+  let currentGap = gap
+  let numItems = 0
+
+  return hourlyData.reduce((accumulator, currentValue) => {
+    if (currentValue.time >= lastRefreshTime && numItems < maxItems) {
+      if (currentGap === gap) {
+        accumulator.push(currentValue)
+        numItems += 1
+      }
+      currentGap -= 1
+      if (currentGap === 0) currentGap = gap
+    }
+
+    return accumulator
+  }, [])
+}
+
 const ExpandedWeatherCard = ({ location }) => {
   const { city, country, currently, hourly, daily } = location
-  const { time, summary, icon, apparentTemperature } = currently
-
-  const currentTime = Date.now()
+  const { time, icon, apparentTemperature, temperature } = currently
 
   return (
     <div className="expanded-weather-card">
+      {/* Date */}
       <section className="date">
         <span>{getFormattedDate(time)}</span>
       </section>
 
+      {/* Time */}
       <section className="time">
-        {moment(currentTime).format('hh:mm')}
-        <span className="time-modifier">{moment(currentTime).format('A')}</span>
+        {moment.unix(time).format('hh:mm')}
+        <span className="time-modifier">{moment.unix(time).format('A')}</span>
       </section>
 
+      {/* Current temperature */}
       <section className="current-temperature">
-        {getTemperature(apparentTemperature)}
+        {getTemperature(apparentTemperature || temperature)}
         &deg;
       </section>
 
+      {/* Location and forecast */}
       <section className="location-and-forecast">
         <section className="location">
           {city}, {country}
         </section>
-        <section className="forecast">{summary}</section>
+        <section className="forecast">{currently.summary}</section>
       </section>
 
-      {/* eslint-disable jsx-a11y/accessible-emoji */}
-      <section className="hourly-forecast">
-        <div className="hourly-temperature-forecast">
-          <div className="time">7pm</div>
-          <div className="temperature">21&deg;</div>
-          <div className="precipitation">
-            <AnimatedWeatherIconsReact icon="RAIN" size={12} />
-            70%
-          </div>
-        </div>
-        <div className="hourly-temperature-forecast">
-          <div className="time">11pm</div>
-          <div className="temperature">19&deg;</div>
-          <div className="precipitation">
-            <AnimatedWeatherIconsReact icon="RAIN" size={12} />
-            75%
-          </div>
-        </div>
-        <div className="hourly-temperature-forecast">
-          <div className="time">2am</div>
-          <div className="temperature">16&deg;</div>
-          <div className="precipitation">
-            <AnimatedWeatherIconsReact icon="RAIN" size={12} />
-            80%
-          </div>
-        </div>
-        <div className="hourly-temperature-forecast">
-          <div className="time">5am</div>
-          <div className="temperature">15&deg;</div>
-          <div className="precipitation">
-            <AnimatedWeatherIconsReact icon="RAIN" size={12} />
-            60%
-          </div>
-        </div>
-        <div className="hourly-temperature-forecast">
-          <div className="time">8am</div>
-          <div className="temperature">18&deg;</div>
-          <div className="precipitation">
-            <AnimatedWeatherIconsReact icon="RAIN" size={12} />
-            45%
-          </div>
-        </div>
-      </section>
+      {/* Hourly forecast section */}
+      <HourlyForecastSection hourly={getHourlyData(hourly.data, time)} />
 
+      {/* Animated weather icon */}
       <section className="animated-weather-icon">
         <AnimatedWeatherIconsReact icon={getIconName(icon)} size={48} />
       </section>
-      {/* eslint-enable */}
 
-      <section className="summary">Drizzle starting in an hour.</section>
+      {/* Summary */}
+      <section className="summary">{hourly.summary}</section>
 
+      {/* Weather animation frame */}
       <section className="weather-animation-frame">Reserved for displaying weather animation frame.</section>
 
+      {/* Daily forecast section */}
       <DailyForecastSection data={daily.data} />
 
-      <WeatherAttributesSection todaysData={daily.data[0]} />
+      {/* Weather attributes section */}
+      <WeatherAttributesSection todaysData={daily.data[0] || {}} />
     </div>
   )
 }
 
 ExpandedWeatherCard.propTypes = {
   location: PropTypes.shape({
-    id: PropTypes.string.isRequired,
     city: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
     country: PropTypes.string.isRequired,
     currently: PropTypes.shape({
       time: PropTypes.number.isRequired,
-      summary: PropTypes.string.isRequired,
       icon: PropTypes.string.isRequired,
       temperature: PropTypes.number.isRequired,
       apparentTemperature: PropTypes.number.isRequired,
     }).isRequired,
     hourly: PropTypes.shape({
+      summary: PropTypes.string.isRequired,
       data: PropTypes.arrayOf(
         PropTypes.shape({
-          precipProbability: PropTypes.number.isRequired,
-          temperature: PropTypes.number.isRequired,
+          time: PropTypes.number.isRequired,
           apparentTemperature: PropTypes.number.isRequired,
+          temperature: PropTypes.number.isRequired,
+          precipProbability: PropTypes.number.isRequired,
         })
       ).isRequired,
     }).isRequired,
@@ -130,11 +113,11 @@ ExpandedWeatherCard.propTypes = {
         PropTypes.shape({
           time: PropTypes.number.isRequired,
           icon: PropTypes.string.isRequired,
-          precipProbability: PropTypes.number.isRequired,
           temperatureHigh: PropTypes.number.isRequired,
           temperatureLow: PropTypes.number.isRequired,
           apparentTemperatureHigh: PropTypes.number.isRequired,
           apparentTemperatureLow: PropTypes.number.isRequired,
+          precipProbability: PropTypes.number.isRequired,
         })
       ).isRequired,
     }).isRequired,
