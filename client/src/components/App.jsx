d3385d6cdff151598c2f2736b607c007a589dab1
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-11-09 01:01:32 +0530

    Refactor code and rehydrate the store on refresh

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
@@ -7,26 +7,43 @@ import WeatherCardsSection from './WeatherCardsSection'
 import ExpandedWeatherCard from './ExpandedWeatherCard'
 import Footer from './Footer'
 import { getCardState } from '../reducers/locations'
+import { fetchAllLocationsData } from '../actions'
 
 import '../styles/app.scss'
 
-const App = ({ cardState }) => (
-  <div className="wrapper">
-    <div className="container">
-      <header className="app-brand">Weather</header>
-      <SearchBar />
-      {(cardState === 'STACKED' && <WeatherCardsSection />) || <ExpandedWeatherCard />}
-    </div>
-    <Footer />
-  </div>
-)
+class App extends Component {
+  componentDidMount() {
+    const { fetchAllLocationsData } = this.props
+
+    // Rehydrate the store once the app has loaded.
+    fetchAllLocationsData()
+  }
+
+  render() {
+    const { cardState } = this.props
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <header className="app-brand">Weather</header>
+          <SearchBar />
+          {(cardState === 'STACKED' && <WeatherCardsSection />) || <ExpandedWeatherCard />}
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
 
 App.propTypes = {
   cardState: PropTypes.string.isRequired,
+  fetchAllLocationsData: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
   cardState: getCardState(state),
 })
 
-export default connect(mapStateToProps)(App)
+export default connect(
+  mapStateToProps,
+  { fetchAllLocationsData }
+)(App)
