23c0c6b48fe80317b9b093d9caccfc8fd7f14eea
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2020-07-09 03:40:48 +0530

    revert: changes to configureUri file

--- client/src/configureUri.js
+++ client/src/configureUri.js
@@ -1,6 +1,9 @@
-const uriConfig = {
-  weatherUri: '/api/weather',
-  autocompleteUri: '/api/autocomplete'
-}
+const uriConfig = process.env.NODE_ENV === 'production' ? {
+  weatherUri: '/weather-app/api/weather',
+  autocompleteUri: '/weather-app/api/autocomplete'
+} : {
+    weatherUri: '/api/weather',
+    autocompleteUri: '/api/autocomplete'
+  }
 
 export default uriConfig
