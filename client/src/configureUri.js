790f33f1e4ffde4427bac697e42f25cf807bf28d
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2020-07-09 15:10:35 +0530

    chore: remove prefix path from url

--- client/src/configureUri.js
+++ client/src/configureUri.js
@@ -1,9 +1,6 @@
-const uriConfig = process.env.NODE_ENV === 'production' ? {
-  weatherUri: '/weather-app/api/weather',
-  autocompleteUri: '/weather-app/api/autocomplete'
-} : {
-    weatherUri: '/api/weather',
-    autocompleteUri: '/api/autocomplete'
-  }
+const uriConfig = {
+  weatherUri: '/api/weather',
+  autocompleteUri: '/api/autocomplete'
+}
 
 export default uriConfig
